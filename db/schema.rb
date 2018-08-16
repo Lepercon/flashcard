5d8a4891b0e1d22acc0f820e04450edeeadc6670
wushingmushine <oisin.brennan@immersivelabs.co.uk>
2018-08-16 15:40:52 +0100

    add password digest to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_16_134714) do
+ActiveRecord::Schema.define(version: 2018_08_16_143833) do
 
   create_table "answers", force: :cascade do |t|
     t.string "answertext"
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 2018_08_16_134714) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
