edce290d653f1661da8cbdedf91821039fed34f6
wushingmushine <oisin.brennan@immersivelabs.co.uk>
2018-08-21 17:05:39 +0100

    User remember me option added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,17 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	user  = User.find_by(email: params[:session][:email].downcase)
-  	if user && user.authenticate(params[:session][:password])
-  		log_in user
-      remember user
-  		redirect_to user
+  	@user = User.find_by(email: params[:session][:email].downcase)
+  	if @user && @user.authenticate(params[:session][:password])
+  		log_in @user
+      if params[:session][:remember_me] == '1'
+        flash[:danger] = "This if clause is running DELETE ME" 
+        remember(@user) 
+      else
+        flash[:danger] = "This else clause is running DELETE ME"
+        forget(@user)
+  		end
+      redirect_to @user
   	else
   		flash.now[:danger] = "Invalid email/ password combination"
   		render 'new'
