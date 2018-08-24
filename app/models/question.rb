6b27f9f3031437aec03c6e61c7363e88ea4dd51a
wushingmushine <oisin.brennan@immersivelabs.co.uk>
2018-08-24 13:07:59 +0100

    Adding new question progress

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,6 @@
 class Question < ApplicationRecord
- has_many :answers
+ has_many :answers, dependent: :destroy
+ accepts_nested_attributes_for :answers, limit: 4, :reject_if => proc {
+  |attributes| attributes['answertext'].blank?
+ } 
 end
